005a25fac7691bb16f4906bfd87cefd492aa4e99
Eduardo Castillo <20243ds106@utez.edu.mx>
2026-06-09 13:09:51 -0600

    style: Correcion de logotipo

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -9,14 +9,14 @@ export function LoginPage() {
                 <img
                     src="/src/assets/logoFederacion.png"
                     alt="Federación Centro Sur"
-                    className="h-12 w-auto object-contain mb-12 self-start"
+                    className="h-30 w-45 object-contain mb-12 self-start"
                 />
 
-                <h1 className="text-4xl font-bold text-gray-900 mb-2">
+                <h1 className="text-4xl font-bold text-[#014A24] mb-2">
                     Iniciar sesión
                 </h1>
                 <p className="text-gray-500 mb-8 text-sm">
-                    ¿No tienes cuenta? <span className="font-medium text-gray-700 cursor-pointer hover:underline">Ponte en contacto con el administrador.</span>
+                    ¿No tienes cuenta? <span className="font-medium text-[#014A24] cursor-pointer hover:underline">Ponte en contacto con el administrador.</span>
                 </p>
 
                 {/* formulario con logica */}
