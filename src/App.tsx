42e4a5138449a0fa9e832f6e5567d8e242536d3e
Eduardo Castillo <20243ds106@utez.edu.mx>
2026-06-10 10:56:44 -0600

    feat: agregue una nuava ruta temporal para la recuperacion de contraseña

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'; // 1. Importamos TanStack Query
 import { LoginPage } from './pages/LoginPage';
 import { RecoverPasswordPage } from './pages/RecoverPasswordPage';
+import { ResetPasswordPage } from './pages/ResetPasswordPage';
 
-// 2. Creamos la instancia del cliente global
+// Instancia del cliente global
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -15,18 +16,18 @@ const queryClient = new QueryClient({
 
 function App() {
   return (
-    // 3. Envolvemos todo con el QueryClientProvider
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Routes>
-          {/* Ruta por defecto que redirige al login */}
+          {/* Ruta por defecto*/}
           <Route path="/" element={<Navigate to="/login" replace />} />
 
-          {/* Nuestras pantallas actuales */}
+          {/* pantallas actuales */}
           <Route path="/login" element={<LoginPage />} />
           <Route path="/recover-password" element={<RecoverPasswordPage />} />
+          <Route path="/reset-password" element={<ResetPasswordPage />} />
 
-          {/* Redirección para rutas inexistentes */}
+          {/* Redirección*/}
           <Route path="*" element={<Navigate to="/login" replace />} />
         </Routes>
       </BrowserRouter>
