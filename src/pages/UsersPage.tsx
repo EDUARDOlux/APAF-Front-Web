2df22fcd885132d8aa6ce28b26d91a0933aad264
Eduardo Castillo <20243ds106@utez.edu.mx>
2026-06-12 15:37:45 -0600

    feat: page para gestion de usarios

---
+++ src/pages/UsersPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { UserTable } from '../features/users/components/UserTable';
+
+export const UsersPage: React.FC = () => {
+  return (
+    <main>
+      <UserTable />
+    </main>
+  );
+};
